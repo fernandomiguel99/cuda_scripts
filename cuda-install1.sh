d9113f8d6014ec06f077144ca7ac52f49e3f3495
ViperV1 <62571922+ViperV1@users.noreply.github.com>
2021-06-23 14:46:00 -0300

    updated cuda-install1

--- cuda-install1.sh
+++ cuda-install1.sh
@@ -9,6 +9,7 @@ cd ~/Downloads/
 wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
+sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda-10-0 -y
 sudo cp cuda.sh /etc/profile.d/cuda.sh 
